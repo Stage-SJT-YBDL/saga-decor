46478ffe0766fcf2874f0e060de29e780356f27c
Bruno <153070299+IncroyableBruno@users.noreply.github.com>
2024-03-15 11:50:10 +0100

    Add files via upload

--- news.php
+++ news.php
@@ -12,15 +12,19 @@
     </section>
     <section class="sec">
         <h2>Pôle Décor</h2>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure,
-             illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe. <br><br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum 
-            delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe. <br><br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.  Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissim
-            os iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe. <br><br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quibusdam alias unde dignissimos iure, illum repellat fuga cum delectus asperiores explicabo possimus veritatis cumque beatae. Illum quas voluptatem assumenda saepe. <br><br>
-        </p>
+            <p>
+                Pôle Décor est une entité qui réunit Saga Décor et Charantaise, deux entreprises spécialisées dans la décoration de bouteilles. Ils offrent des services de satinage et de laquage de bouteilles, permettant ainsi de créer des designs uniques et attrayants.
+                <br><br>
+                Saga Décor est réputée pour son expertise dans la décoration de bouteilles. Leur équipe talentueuse utilise des techniques avancées de satinage pour créer des finitions élégantes et texturées sur les bouteilles. Le satinage donne un aspect doux et velouté, ajoutant une touche de sophistication à chaque bouteille. Les artisans de Saga Décor sont attentifs aux détails et veillent à ce que chaque bouteille soit traitée avec soin et précision.
+
+                <br>
+                D'autre part, Charantaise se spécialise dans le laquage de bouteilles. Ils appliquent des couches de laque de haute qualité pour créer des finitions lisses et brillantes. Le laquage donne aux bouteilles un aspect raffiné et attrayant, mettant en valeur les couleurs et les motifs choisis. Les professionnels de Charantaise maîtrisent l'art du laquage et travaillent avec minutie pour obtenir des résultats impeccables.
+                <br><br>
+                En combinant les compétences de Saga Décor et de Charantaise, Pôle Décor offre une gamme complète de services de décoration de bouteilles. Que ce soit pour des bouteilles de vin, de champagne, de spiritueux ou d'autres produits, ils peuvent transformer chaque bouteille en une pièce unique et personnalisée. Leur savoir-faire et leur attention aux détails leur permettent de répondre aux besoins et aux exigences des clients les plus exigeants.
+
+                Grâce à leur expertise et à leur passion pour la décoration de bouteilles, Pôle Décor se positionne comme un acteur majeur dans l'industrie. Leur objectif est de créer des designs de bouteilles exceptionnels qui captivent l'attention des consommateurs et renforcent l'image de marque des produits.
+               
+            </p>
        
     </section>
     <div class="ima"></div> 
@@ -30,7 +34,7 @@
     <div class="mesbox flex justify-around align-center"> 
         <div class="articles pd2 justify-center align-center">
              <div class="box justify-center">
-                <img class="im1" src="./assets/media/img/accueil2/v6.png" width="400px" height="230px" alt="image">
+                <img class="im1" src="./assets/media/img/accueil2/v6.png"  width="400px" height="230px" alt="image">
             </div>
              <div>
                 <p class="laquar flex justify-center align-center  color-white ">Laquage de bouteillesLaquage de bouteilles
@@ -86,5 +90,5 @@
         </div> 
     </div>     
 <?php
- include('footer.php');
+ include('footer_news.php');
 ?>
