259f7546840cf339b631ce84c104a8031b290ee7
Bruno <153070299+IncroyableBruno@users.noreply.github.com>
2024-03-27 14:14:05 +0100

    Add files via upload

--- news.php
+++ news.php
@@ -2,13 +2,8 @@
  include('header.php');
 ?>
 
-    <section class="body parallax">
-        <img src="./assets/media/img/accueil2/yy.png" alt="image" id="hill4">
-        <img src="./assets/media/img/accueil2/ll.png" alt="image" id="hill5">
-        <h2 id="text">Saga Décor</h2>
-        <img src="./assets/media/img/accueil2/zz.png" alt="image" id="tree">
-        <img src=""  id="leaf">
-        <img src=""  id="plant">
+    <section>
+    <video id="vid" class="vi-saga" src="./assets/media/vid/video-saga.webm" style="width: 100%;" autoplay muted loop></video>
     </section>
     <section class="sec">
         <h2>Pôle Décor</h2>
@@ -28,12 +23,12 @@
     </section>
     <div class="ima"></div> 
     <section class="Actualités">
-        <h1 class="act flex color-white justify-center">Actualités</h1>
+        <h1 class="act flex  justify-center">Actualités</h1>
     </section>
     <div class="mesbox flex justify-around align-center"> 
         <div class="articles pd2 justify-center align-center">
              <div class="box justify-center">
-                <img class="im1" src="./assets/media/img/accueil2/v6.png"  width="400px" height="230px" alt="image">
+                <img class="im1" src="./assets/media/img/sagadecor/biere-chti-saison-brasserie-castelain-biere-de-garde-saga-decor-serigraphie-email-actualite.jpg" width="400px" height="230px" alt="image">
             </div>
              <div>
                 <p class="laquar flex justify-center align-center  color-white ">Laquage de bouteillesLaquage de bouteilles
@@ -46,12 +41,12 @@
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nostrum inventor </p>
             </div>
              <div class="box justify-center">
-                <img class="im2" src="./assets/media/img/accueil2/p.png" width="400px" height="230px" alt="image2">
+                <img class="im2" src="./assets/media/img/sagadecor/alalia-546x390.jpg" width="400px" height="230px" alt="image2">
             </div>
        </div>
        <div class="articles pd2 justify-center align-center">
             <div class="box justify-center">
-                <img class="im1" src="./assets/media/img/accueil2/verr3.jpg" width="400px" height="230px" alt="image">
+                <img class="im1" src="./assets/media/img/sagadecor/belle-epoque-2004-en-coffret6-546x390.jpg" width="445px" height="230px" alt="image">
             </div>
             <div>
                 <p class="laquar flex justify-center align-center color-white ">Laquage de bouteillesLaquage de bouteilles
@@ -62,7 +57,7 @@
     <div class="mesbox flex justify-around align-center"> 
         <div class="articles pd2 justify-center align-center">
              <div class="box justify-center">
-                <img class="im1" src="./assets/media/img/accueil2/v6.png" width="400px" height="230px" alt="image">
+             <img class="im1" src="./assets/media/img/sagadecor/biere-chti-saison-brasserie-castelain-biere-de-garde-saga-decor-serigraphie-email-actualite.jpg" width="400px" height="230px" alt="image">
             </div>
              <div>
                 <p class="laquar flex justify-center align-center  color-white ">Laquage de bouteillesLaquage de bouteilles
@@ -75,12 +70,12 @@
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nostrum inventor </p>
             </div>
              <div class="box justify-center">
-                <img class="im2" src="./assets/media/img/accueil2/p.png" width="400px" height="230px" alt="image2">
+                <img class="im2" src="./assets/media/img/sagadecor/belle-epoque-2004-en-coffret6-546x390.jpg"  width="400px" height="300px">
             </div>
        </div>
        <div class="articles pd2 justify-center align-center">
             <div class="box justify-center">
-                <img class="im1" src="./assets/media/img/accueil2/verr3.jpg" width="400px" height="230px" alt="image">
+                <img class="im1" src="./assets/media/img/sagadecor/alalia-546x390.jpg" width="400px" height="230px" alt="image">
             </div>
             <div>
                 <p class="laquar flex justify-center align-center color-white ">Laquage de bouteillesLaquage de bouteilles
